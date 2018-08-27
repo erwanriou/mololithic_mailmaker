1b9cb74475ac715e7ce5635eed8769fd1da78d74
Erwan Riou <rr.1.riou@gmail.com>
2018-08-27 19:22:38 +0200

    setup deploy on heroku 4

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
 
 // Server Port
 const PORT =  process.env.PORT || 5000
-app.listen(port, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

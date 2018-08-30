d4c611c886ef5eb471c0f98afb2e09aa44cadf4d
Erwan Riou <rr.1.riou@gmail.com>
2018-08-30 13:06:39 +0200

    [Project] - Deploying to heroku 4

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,6 +2,7 @@ const keys = {
   secret: process.env.SECRET_OR_KEY,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_SECRET_OR_KEY,
+  cookieKey: process.env.COOKIE_KEY,
   url : function() {
     return process.env.MONGO_URI
   },

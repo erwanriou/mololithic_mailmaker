dbc8bd86522327e4ccbc76d833e86a609e3b8bfa
Erwan <rr.1.riou@gmail.com>
2018-09-05 09:32:46 +0200

    [Project] - Test deploy 13 on heroku

--- server.js
+++ server.js
@@ -19,19 +19,6 @@ const keys = require('./config/keys').keys
 const db = require('./config/keys').keys
 
 // Middleware
-app.use(function(req, res, next) {
-        if (process.env.NODE_ENV === "production") {
-            const reqType = req.headers["x-forwarded-proto"];
-            // if not https redirect to https unless logging in using OAuth
-            if (reqType !== "https") {
-                req.url.indexOf("auth/google") !== -1
-                  ? next()
-                  : res.redirect("https://" + req.headers.host + req.url);
-            }
-        } else {
-            next();
-        }
-    });
 app.use(cookieSession({ maxAge: 24 * 60 * 60 * 1000,  keys: [keys.cookieKey] }))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())

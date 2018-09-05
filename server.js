7827ab49f8cf3e1df1fd1c422af2718aad1e5cc7
Erwan <rr.1.riou@gmail.com>
2018-09-05 08:53:44 +0200

    [Project] - Test deploy 8 on heroku

--- server.js
+++ server.js
@@ -24,7 +24,19 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(passport.session())
-
+app.use(function(req, res, next) {
+        if (process.env.NODE_ENV === "production") {
+            const reqType = req.headers["x-forwarded-proto"];
+            // if not https redirect to https unless logging in using OAuth
+            if (reqType !== "https") {
+                req.url.indexOf("auth/google") !== -1
+                  ? next()
+                  : res.redirect("https://" + req.headers.host + req.url);
+            }
+        } else {
+            next();
+        }
+    }); 
 // Passport config
 require('./services/passportGoogle')(passport)
 require('./services/passportJwt')(passport)

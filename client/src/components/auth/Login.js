82103ce0893831cb4434c8df8a334cee7c4e35d0
Erwan <rr.1.riou@gmail.com>
2018-09-05 07:56:25 +0200

    [Project] - Test deploy on heroku

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -9,7 +9,7 @@ class Login extends React.Component {
           <div className="container">
             <div className="title">
               <h1> Login in the application</h1>
-              <h2> You can choose by sign in with google, facebook or password authentification</h2>
+              <h2> You can choose by Sign-In with google, facebook or password authentification</h2>
             </div>
             <div className="login">
               <a href='/auth/google' className="google">

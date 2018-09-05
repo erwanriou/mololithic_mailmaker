140ac3f26b9fd82c786cbd0b692c29384be03630
Erwan <rr.1.riou@gmail.com>
2018-09-05 09:18:40 +0200

    [Project] - Test deploy 11  on heroku

--- server.js
+++ server.js
@@ -46,19 +46,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-app.use(function(req, res, next) {
-        if (process.env.NODE_ENV === "production") {
-            const reqType = req.headers["x-forwarded-proto"];
-            // if not https redirect to https unless logging in using OAuth
-            if (reqType !== "https") {
-                req.url.indexOf("auth/google") !== -1
-                  ? next()
-                  : res.redirect("https://" + req.headers.host + req.url);
-            }
-        } else {
-            next();
-        }
-    });
 
 // Server Port
 const port =  process.env.PORT || 5000

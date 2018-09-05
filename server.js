046b028624e5ca85537b4677c66670abc71560a9
Erwan <rr.1.riou@gmail.com>
2018-09-05 08:03:16 +0200

    [Project] - Test deploy 2 on heroku

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.use('/auth/google', google)
 app.use('/api/users', users)
 
 // Production Setup
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'))
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Server Port
 const port =  process.env.PORT || 5000

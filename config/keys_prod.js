36631feb5ef8d710c6c70a911041d159fd6791b7
Erwan <rr.1.riou@gmail.com>
2018-09-05 18:52:20 +0200

    [Project] - Implementing stripe payment keys on backend side

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,6 +2,8 @@ const keys = {
   secret: process.env.SECRET_OR_KEY,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_SECRET_OR_KEY,
+  stripePublishableKey: process.env.STRIPE_CLIENT_ID,
+  stripeSecretKey: process.env.STRIPE_SECRET_OR_KEY,
   cookieKey: process.env.COOKIE_KEY,
   url : function() {
     return process.env.MONGO_URI

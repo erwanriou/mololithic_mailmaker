9f347972cf26e2e6ab432fd16b42c79cba8debc8
Erwan <rr.1.riou@gmail.com>
2018-09-11 13:49:25 +0200

    [Project] - Implement Credit check condition on frontend

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,30 @@ import Payments from '../transaction/Payments'
 class Dashboard extends React.Component {
   render() {
     const  { user } = this.props.auth
+    let creditCheck
+
+    user.credits > 1
+      ? creditCheck = (
+          <Link className='createsurvey' to='/dashboard/new'>
+            <i className="fas fa-pen"></i>
+            <div className='content'>
+              <h2>New Survey</h2>
+              <p>Click here to create a new marketing email campaign</p>
+              <p>It will cost you 1 credit of yours {user.credits} credit(s) and you will have {user.credits -1} credit(s)</p>
+            </div>
+          </Link>
+        )
+      : creditCheck = (
+          <div className='createsurvey'>
+            <i class="fas fa-exclamation"></i>
+            <div className='content'>
+              <h2>You don't have enough credit to create a New Survey</h2>
+              <p>Buy more credit before by clicking 'add credits' and don't worry, payment is in test mode</p>
+              <p>Use the credit card 4242 4242 4242 4242 to pay!</p>
+            </div>
+          </div>
+        )
+
     return (
       <Fragment>
         <div className="submenu">
@@ -24,13 +48,9 @@ class Dashboard extends React.Component {
         <main className='dashboard'>
           <div className="opacity">
             <div className="container">
-              <Link className='createsurvey' to='/dashboard/new'>
-                <i className="fas fa-pen"></i>
-                <div className='content'>
-                  <h2>New Survey</h2>
-                  <p>Click here to create a new marketing email campaign</p>
-                </div>
-              </Link>
+
+                {creditCheck}
+
             </div>
           </div>
         </main>
